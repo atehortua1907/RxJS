3122751632e975148a421dafbfe16fdaeabb2bf4
David Atehortua <d.atehortua@sana-commerce.com>
2022-01-04 16:20:15 -0500

    Funciones para crear Observables => of

--- 1-App/src/index.ts
+++ 1-App/src/index.ts
@@ -1,28 +1,2 @@
-import { fromEvent } from "rxjs";
+import { range } from "rxjs";
 
-/**
- * Eventos del DOM
- */
-
-const src1$ = fromEvent<MouseEvent>(document, 'click');
-const src2$ = fromEvent<KeyboardEvent>(document, 'keyup');
-
-const observer = {
-    next: val => console.log('next', val)
-};
-
-//Imprimir las coordenadas del evento click
-
-// src1$.subscribe(evt => {
-//     console.log(evt.x);
-//     console.log(evt.y);
-// });
-
-//Con destrecturación
-src1$.subscribe(({x,y}) => {
-    console.log(x, y);
-
-});
-src2$.subscribe(evento => {
-    console.log(evento.key);
-});
