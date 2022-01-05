ee10f6fd5de39fb45b60df4ba2c3b535081f8c3a
David Atehortua <d.atehortua@sana-commerce.com>
2022-01-05 14:55:19 -0500

    Operadores basicos => mapTo

---
+++ 1-App/src/operators/01-map-pluck-mapTo.ts
@@ -0,0 +1,32 @@
+import { fromEvent, range, Subscriber } from "rxjs";
+import { map, pluck, mapTo } from 'rxjs/operators';
+
+// range(1,5).pipe(
+//     map<number,string>( val => (val * 10).toString())
+// ).subscribe(console.log);
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+//map: Transforma el objeto
+const keyupEvent$ = keyup$.pipe(
+    map(event => event.code)
+);
+
+//pluck: extrae la propiedad indicada por parametro
+//si necesito una propiedad interna, navego separado por comas, ejemplo:
+//propiedad 'baseURI' del objeto 'target' del objeto que se recibe por el observable
+const keyupPluck$ = keyup$.pipe(
+    pluck('target','baseURI')
+);
+
+//mapTo: devuelve cualquier cosa (string, objetos, nativos, etc)
+const keyupMapTo$ = keyup$.pipe(
+    mapTo('tecla presionada')
+);
+
+//Debo de subscribirme al observable que retorna el pipe
+
+keyup$.subscribe(console.log);
+keyupEvent$.subscribe(val => console.log('map =', val));
+keyupPluck$.subscribe(val => console.log('pluck =', val));
+keyupMapTo$.subscribe(val => console.log('mapTo =', val));
